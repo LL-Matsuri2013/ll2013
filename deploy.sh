bc32898a482d2072bf6221475ec584a00f60ecf8
IWAI, Masaharu <iwaim.sub@gmail.com>
2013-08-03 01:44:54 +0900

    rsyncのオプションを変数に抜き出した

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+RSYNC_OPTS="-av --delete"
+
 if [ $# -ne 1 ]; then
   echo "please use $0 [jus|smellman]"
   exit 1
@@ -8,7 +11,7 @@ if [ $1 = "jus" ]; then
   echo "deploy to ll.jus.or.jp/2013"
   rm -fr ../deploy/ll.jus
   bundle exec jekyll --base-url http://ll.jus.or.jp/2013 ../deploy/ll.jus
-  rsync -av --delete ../deploy/ll.jus/ ll.jus.or.jp:/usr/local/docs/ll.jus.or.jp/llweb/2013
+  rsync $RSYNC_OPTS ../deploy/ll.jus/ ll.jus.or.jp:/usr/local/docs/ll.jus.or.jp/llweb/2013
   exit $?
 fi
 
@@ -16,7 +19,7 @@ if [ $1 = "smellman" ]; then
   echo "deploy to ll2013.smellman.org"
   rm -fr ../deploy/smellman.org
   bundle exec jekyll --base-url http://ll2013.smellman.org ../deploy/smellman.org
-  rsync -av --delete ../deploy/smellman.org/ synthamesk:/home/nginx/ll2013
+  rsync $RSYNC_OPTS ../deploy/smellman.org/ synthamesk:/home/nginx/ll2013
   exit $?
 fi
 
