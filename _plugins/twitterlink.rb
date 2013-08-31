02545bfd53a49886dddcf089e9514119eb2e0a52
IWAI, Masaharu <iwaim.sub@gmail.com>
2013-09-01 02:20:31 +0900

    プラグイン追加: TwitterLink
    twitterタグでツイッターアカウントへのリンクを生成

---
+++ _plugins/twitterlink.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+module Jekyll
+  class TwitterLink < Liquid::Tag
+    def initialize(tag_name, screen_name, tokens)
+      super
+      @screen_name = screen_name
+    end
+
+    def render(context)
+      sprintf("<a href=\"https://twitter.com/%s\">@%s</a>", @screen_name, @screen_name)
+    end
+  end
+end
+
+Liquid::Template.register_tag("twitter", Jekyll::TwitterLink)
